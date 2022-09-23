c8188375271c4703b5ff5e25317baaf1770edc86
Student <Student@172.16.1.158>
2022-09-23 14:48:04 +0700

    first commit

---
+++ KTPM_Tuan4_QuyKha/src/main/java/com/example/KTPM_Tuan4_QuyKha/KtpmTuan4QuyKhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.KTPM_Tuan4_QuyKha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtpmTuan4QuyKhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtpmTuan4QuyKhaApplication.class, args);
+	}
+
+}
---
+++ KTPM_Tuan4_QuyKha/src/test/java/com/example/KTPM_Tuan4_QuyKha/KtpmTuan4QuyKhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KTPM_Tuan4_QuyKha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpmTuan4QuyKhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
